6c1be15b0bac923bdf5f0f2a81d30c55684648e3
Carlos Lopez <c.lopez@kmels.net>
2018-08-01 03:21:47 +0000

    - Support cash checkpoints - Bump to 0.14.8

--- core/src/main/java/org/bitcoinj/core/CheckpointManager.java
+++ core/src/main/java/org/bitcoinj/core/CheckpointManager.java
@@ -108,7 +108,10 @@ public class CheckpointManager {
 
     /** Returns a checkpoints stream pointing to inside the bitcoinj JAR */
     public static InputStream openStream(NetworkParameters params) {
-        return CheckpointManager.class.getResourceAsStream("/" + params.getId() + ".checkpoints.txt");
+        return CheckpointManager.class.getResourceAsStream("/"
+                + params.getId()
+                + (params.getUseForkId() ? ".cash" : "")
+                + ".checkpoints.txt");
     }
 
     private Sha256Hash readBinary(InputStream inputStream) throws IOException {
